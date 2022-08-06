5227b6cfe1bae3e639f8b1675787b07bfd52a34b
ram yewale <ramn.yewale@gmail.com>
2022-08-06 16:35:37 +0530

    One more commit on dev branch.

--- Jenkins-CICD/src/main/java/com/java/sprin/cicd/jenkins/controller/ProductController.java
+++ Jenkins-CICD/src/main/java/com/java/sprin/cicd/jenkins/controller/ProductController.java
@@ -9,7 +9,7 @@ public class ProductController {
 
     @GetMapping("/jenkins-application")
     public String getResponse(){
-        System.out.println("Product controller api triggered");
-        return "Welcome to first Jenkins CICD project.";
+        System.out.println("Product controller api triggered...!!");
+        return "Welcome to first Jenkins CICD project...!!!!";
     }
 }
